c2c81d439a0f2f2957843d64831072448ec3fc5f
yousuf bin tashfeen <yousufbintashfeen549@gmail.com>
2026-05-20 10:40:39 -0700

    Updated stock market project

--- Apple Stock Market Data Analysis and Visualization System Using Python Libraries.py
+++ Apple Stock Market Data Analysis and Visualization System Using Python Libraries.py
@@ -3,19 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mplfinance.original_flavor import candlestick_ohlc
 import matplotlib.dates as mpdates
+
 reads = pd.read_csv('AAPL.csv')
-print(reads.iloc[1])
+
 reads = reads.round(2)
-print(reads.iloc[0])
+
 reads['Date'] = pd.to_datetime(reads['Date'])
+
 average_price = np.mean(reads['Close']).round(2)
 highest_price = np.max(reads['High']).round(2)
 lowest_price = np.min(reads['Low']).round(2)
+
 print("Average Closing Price:", average_price)
 print("Highest Stock Price:", highest_price)
 print("Lowest Stock Price:", lowest_price)
+
 reads['Date'] = reads['Date'].map(mpdates.date2num)
+
 data = reads[['Date', 'Open', 'High', 'Low', 'Close']]
+
 fig, ax = plt.subplots(figsize=(12,6))
 
 candlestick_ohlc(
@@ -29,15 +35,18 @@ candlestick_ohlc(
 plt.text(
     0.02,
     0.95,
-    f'Average: {average_price:.2f}\nHighest: {highest_price:.2f}\nLowest: {lowest_price:.2f}',
+    f'Average: {average_price}\nHighest: {highest_price}\nLowest: {lowest_price}',
     transform=ax.transAxes,
     fontsize=10,
     verticalalignment='top'
 )
 
 ax.xaxis_date()
+
 plt.title('Apple Stock Candlestick Chart')
 plt.xlabel('Date')
 plt.ylabel('Stock Price')
+
 plt.xticks(rotation=45)
+
 plt.show()
